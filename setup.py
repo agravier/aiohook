cc5fae58dc5eafc511a7a2439f15f4ba484a4fe2
Alexandre Gravier <al.gravier@gmail.com>
2019-08-30 03:43:11 +0800

    Improve buils-and-upload script

--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python
-
-import os
+import glob
+import subprocess
 import sys
 
 from setuptools import setup
 
 if __name__ == "__main__":
     if sys.argv[-1] == "publish":
-        os.system("python setup.py bdist_wheel upload --sign")
+        subprocess.run(["python", "setup.py", "sdist"], check=True)
+        subprocess.run(["twine", "upload"] + glob.glob("dist/*"),
+                       check=True)
         sys.exit()
 
 with open("README.md") as readme:
